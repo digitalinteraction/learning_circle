75ce0a4a770d4438953470903f386ab0f9c2bbb8
Mariusz Przodała <mariusz.przodala@proserwit.pl>
2016-03-23 16:38:27 +0100

    refactoring

--- settings/server/ServiceConfiguration.js
+++ settings/server/ServiceConfiguration.js
@@ -6,7 +6,6 @@
  */
 
 if (process.env.NODE_ENV === 'development') {
-    //facebook app created by Maciek, works on localhost only
     ServiceConfiguration.configurations.remove({
         service: 'facebook'
     });
@@ -16,21 +15,16 @@ if (process.env.NODE_ENV === 'development') {
         secret: Meteor.settings.private.development.FbSecret
     });
 
-    //google app created by Maciek, works on localhost
     ServiceConfiguration.configurations.remove({
         service: 'google'
     });
     ServiceConfiguration.configurations.insert({
         service: 'google',
-        // needed for schedules google calendar events testing
-        // it is on vazcodev@gmail.com
         clientId: Meteor.settings.private.development.GoogleClientId,
         secret: Meteor.settings.private.development.GoogleSecret
-        // original: clientId: '88337990440-our8galvacje54jk26os94md1aro4kar.apps.googleusercontent.com',
-        // original: secret: 'UpAHAwwLZru1vzT8Vozdq-2M'
+
     });
 } else {
-    //facebook app on facebook@elggdev.com
     ServiceConfiguration.configurations.remove({
         service: 'facebook'
     });
@@ -39,18 +33,14 @@ if (process.env.NODE_ENV === 'development') {
         appId: Meteor.settings.public.facebookAppId,
         secret: Meteor.settings.private.facebookAppSecret
     });
-
-    //google app on vvazco@gmail.com account
+    
     ServiceConfiguration.configurations.remove({
         service: 'google'
     });
     ServiceConfiguration.configurations.insert({
         service: 'google',
-        // it is on vazcodev@gmail.com
-        // needed for schedules google calendar events testing
         clientId: Meteor.settings.private.googleClientId,
         secret: Meteor.settings.private.googleSecret
-        // original: clientId: '601756276624-l1a2bg2cbbfjdjgtuu34u7f0grndhlvu.apps.googleusercontent.com',
-        // original: secret: 'NVvhJSwOgs60w0qqawhjPEFk'
+
     });
 }
