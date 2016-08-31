0ac01320ba4d90d2a7bfff7feab721cb65177eed
Mariusz Przodała <mariusz.przodala@proserwit.pl>
2016-08-31 13:54:14 +0200

    fix for mentors tab

--- mods/groups/server/publications.js
+++ mods/groups/server/publications.js
@@ -9,7 +9,7 @@ Meteor.publish('courseGroups', function (courseId) {
 
 Meteor.publish('courseMembers', function (courseId) {
     if (Roles.userIsInRole(this.userId, ['global_student', 'global_mentor', 'global_admin', 'global_community_admin'], courseId)) {
-        var users = Meteor.users.find({courses: courseId});
+        var users = Meteor.users.find({$or:[{courses: courseId},{mentorshipCourses: courseId}]});
         return [
             users,
             ProfileAvatar.find({'metadata.owner':{$in: users.map(function(u){ return u._id; })}})
